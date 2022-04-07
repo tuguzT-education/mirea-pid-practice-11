bf2296717372eb507c2daca3f52ad0fbd7650a54
tuguzT <timurka.tugushev@gmail.com>
2022-04-07 14:01:26 +0300

    Add fragments and logic

--- app/src/main/java/io/github/tuguzt/dialogfragments/MainActivity.kt
+++ app/src/main/java/io/github/tuguzt/dialogfragments/MainActivity.kt
@@ -2,10 +2,18 @@ package io.github.tuguzt.dialogfragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.github.tuguzt.dialogfragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fragmentContainer.setOnClickListener {
+            TestCheckboxDialog().show(supportFragmentManager, TestCheckboxDialog::class.simpleName)
+        }
     }
 }
